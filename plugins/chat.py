229a375e7c19bd0f898458251d4596a96be309e7
barneygale <barney.gale@gmail.com>
2019-05-02 20:36:52 +0100

    Add /minebnc command.

--- plugins/chat.py
+++ plugins/chat.py
@@ -1,4 +1,6 @@
+from twisted.internet import reactor
 from plugins import Plugin
+
 
 
 class ChatPlugin(Plugin):
@@ -16,6 +18,13 @@ class ChatPlugin(Plugin):
                 self.bt.pack('b', 0))
         del self.messages[:]
 
+    def command(self, subcommand=None, *args):
+        if subcommand is None:
+            return "subcommands: stop"
+        elif subcommand == "stop":
+            reactor.stop()
+
+
     def packet_downstream_chat_message(self, buff):
         message = buff.unpack_chat()
         position = buff.unpack('b')
@@ -23,3 +32,14 @@ class ChatPlugin(Plugin):
             self.messages.append(message)
             if len(self.messages) > self.scrollback_limit:
                 self.messages.pop(0)
+
+    def packet_upstream_chat_message(self, buff):
+        request = buff.unpack_string()
+        args = request.split()
+        if args[0] == "/minebnc":
+            response = self.command(*args[1:])
+            self.downstream.send_packet(
+                "chat_message",
+                self.bt.pack(0),
+                self.bt.pack_chat(response))
+            return True
